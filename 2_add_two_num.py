523b97ff155705e2e587d52df120ca542ac9f255
reber <rouwei545@outlook.com>
2021-01-04 18:57:47 +0800

    add two number

--- 2_add_two_num.py
+++ 2_add_two_num.py
@@ -1,25 +1,50 @@
-def AddTwoNumber(list1,list2):
-    list1.reverse()
-    list2.reverse()
-    list1.append(0)
-    list2.append(0)
-    list3 = []
-    a = len(list1)
-    b = 0
-    for i in range(0,a):
-        c = list1[i]+list2[i]+b
-        b = list1[i]+list2[i]+b
-        b = b%10
-        list3.append(b)
-        b = int(c/10)
-    if(list3[a-1]==0):
-        list3.pop()
+# add two number 
+# 1. maybe two list is different length 
+# 2. maybe there is a carry
+
+def XchgList(ls1,ls2):
+    if(len(ls1)>len(ls2)):
+        pass
+    else:
+        ls1,ls2=ls2,ls1
+
+def AppendListWith0(length,ls):
+    if(len(ls)>length):
+        print("the length is two long")
+    elif(len(ls)<length):
+        for i in range(0,length-len(ls)):
+            ls.append(0)
     else:
         pass
-    return (list3.reverse())
 
-list1 = [1,2,3,4]
-list2 = [1,2,3,4]
+        
+def AddTwoNumber(ls1,ls2):
+    ls3 = []
+    k = 0
+    XchgList(ls1,ls2)
+    AppendListWith0(len(ls1)+1,ls1)
+    AppendListWith0(len(ls1)+1,ls2)
+    for i in range(0,len(ls1)):
+        w = k
+        j = ls1[i]+ls2[i]+w
+        if(int(j/10) == 1):
+            j = j%10
+            k = 1
+        else:
+            k = 0
+        ls3.append(j)
+    if (ls3[-1] == 0):
+        ls3.pop()
+    else:
+        pass 
+    return ls3
 
-print(AddTwoNumber(list1,list2))
+ls1 = [9,9,9,9,9]
+ls2 = [1]
+
+print(AddTwoNumber(ls1,ls2))
+
+        
+
+    
 
